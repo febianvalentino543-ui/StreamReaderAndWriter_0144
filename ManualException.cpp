5ade355f2b93fcb14d862dac3567e3e7f356f18b
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-06-02 17:33:10 +0700

    menambahkan blok catch

--- ManualException.cpp
+++ ManualException.cpp
@@ -9,3 +9,7 @@ int main()
         cout << "Selamat belajar di prodi TI UMY" << endl;
         //throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+
+    //kondisi jika throw yang bertipe data integer
+    catch (int a)
